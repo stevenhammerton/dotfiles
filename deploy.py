1538623526d5d98263a52f381007503893f741e5
Steven Hammerton <steven.hammerton@openmarket.com>
2016-10-24 12:55:05 +0100

    Add a deploy script

---
+++ deploy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import os
+
+def link_if_not_exists(sourcePath, linkPath):
+    absoluteSourcePath = os.path.expanduser(sourcePath)
+    absoluteLinkPath = os.path.expanduser(linkPath)
+
+    if (not os.path.exists(absoluteLinkPath)):
+        os.symlink(absoluteSourcePath, absoluteLinkPath)
+
+
+# Profile & Bash
+link_if_not_exists("~/dotfiles/bash/profile",    "~/.profile")
+link_if_not_exists("~/dotfiles/bash/bashrc",     "~/.bashrc")
+
+# Vim
+link_if_not_exists("~/dotfiles/vim/vimrc",     "~/.vimrc")
