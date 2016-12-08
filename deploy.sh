c19dacb6a8e1322c90193cf8051aec1295e651e2
Steven Hammerton <(none)>
2016-12-08 21:43:43 +0000

    Add IntelliJ files

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,9 @@ createLink ~/dotfiles/vim/vimrc ~/.vimrc
 createLink ~/dotfiles/git/gitconfig ~/.gitconfig
 createLink ~/dotfiles/ssh/config ~/.ssh/config
 createLink ~/dotfiles/macos-dock/com.apple.dock.plist ~/Library/Preferences/com.apple.dock.plist
+mkdir -p ~/Library/Preferences/IdeaIC2016.1
+createLink ~/dotfiles/intellij/codestyles ~/Library/Preferences/IntelliJIdea2016.3/codestyles
+createLink ~/dotfiles/intellij/inspection ~/Library/Preferences/IntelliJIdea2016.3/inspection
 
 # Install fonts
 cp ./fonts/*.ttf /Library/Fonts
