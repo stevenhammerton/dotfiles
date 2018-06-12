31bc2ddfe1c4958f4ee6473898346f7b5b8bcf69
Steven Hammerton <(none)>
2018-06-12 14:31:51 +0100

    Cleanup no longer needed things

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,6 @@ createLink ~/dotfiles/bash/bashrc ~/.bashrc
 createLink ~/dotfiles/vim/vimrc ~/.vimrc
 createLink ~/dotfiles/git/gitconfig ~/.gitconfig
 createLink ~/dotfiles/ssh/config ~/.ssh/config
-createLink ~/dotfiles/macos-dock/com.apple.dock.plist ~/Library/Preferences/com.apple.dock.plist
-mkdir -p ~/Library/Preferences/IdeaIC2016.1
-createLink ~/dotfiles/intellij/codestyles ~/Library/Preferences/IntelliJIdea2016.3/codestyles
-createLink ~/dotfiles/intellij/inspection ~/Library/Preferences/IntelliJIdea2016.3/inspection
 
 # Install fonts
 cp ./fonts/*.ttf /Library/Fonts
